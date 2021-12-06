7c8545e8492962988c3577fb67c36a15481db25a
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-12-06 17:34:47 +0700

    Create index.1.js

---
+++ Message/Connect/index.1.js
@@ -0,0 +1,10 @@
+module.exports = {
+Ctx: {
+  serializeM: require("../library/Assets/serialize.js"), 
+  _SCMD: require("../library/Function/SCMD.js"),
+  _moment: require("../library/Assembly/moment.js"),
+  _UserDB: require("./index.2.js"), 
+  _write: require("./index.3.js"), 
+  _classFt: require("./index.4.js")
+  }
+}
